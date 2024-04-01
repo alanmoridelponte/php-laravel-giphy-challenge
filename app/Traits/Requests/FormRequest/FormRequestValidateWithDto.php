bb276d93d1ddb0cbbb1aff2d2610f57471e42125
Alan Mori Delponte <alan@odiseasoft.com>
2024-04-01 11:48:24 -0300

    feat: add runtime check for dto

--- app/Traits/Requests/FormRequest/FormRequestValidateWithDto.php
+++ app/Traits/Requests/FormRequest/FormRequestValidateWithDto.php
@@ -3,9 +3,15 @@
 namespace App\Traits\Requests\FormRequest;
 
 use App\DTOs\BaseDTO;
+use RuntimeException;
 
 trait FormRequestValidateWithDto {
     public function validated($key = null, $default = null): BaseDTO {
+
+        if (!property_exists($this, 'dto') || !is_subclass_of($this->dto, BaseDTO::class)) {
+            throw new RuntimeException('DTO property is missing or is not an instance of BaseDTO');
+        }
+
         return $this->dto::create(
             parent::validated($key, $default)
         );
